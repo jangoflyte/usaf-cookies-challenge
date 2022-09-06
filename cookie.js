c4a5cc45aaee1485c9906e0e6314582f3e5148ee
jangoflyte <miguelpcastro95@gmail.com>
2022-09-06 13:04:38 -0600

    done

---
+++ cookie.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+app.use(express.json());
+var cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.get("/", function (req, res) {
+  var opts = {
+    maxAge: 900000,
+    httpOnly: true
+  };
+  res.cookie('some_name', 'some_value', opts);
+  console.log("Cookies: ", req.cookies);
+  res.send("Cookies challenge!");
+});
+
+
+app.get("/login", (req, res) => {
+    res.cookie("name", "miguel")
+    console.log("Cookies: ", req.cookies);
+    res.send("Cookies challenge!")
+});
+
+app.get("/hello", (req, res) => res.send(`Welcome miguel!`));
+
+app.listen(port, () =>
+  console.log(`Listening at http://localhost:${port}`)
+);
